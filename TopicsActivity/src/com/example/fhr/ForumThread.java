28db3f317cec236fed0bacb55039f738eebd11a8
epson121 <traxdater12@gmail.com>
2013-02-21 16:30:21 +0100

    Added eclipse workspace

---
+++ TopicsActivity/src/com/example/fhr/ForumThread.java
@@ -0,0 +1,21 @@
+package com.example.fhr;
+
+
+public class ForumThread {
+
+	public ForumThread(){}
+	
+	protected int id;
+	protected String threadName;
+	protected String threadUrl;
+	protected String threadAuthor;
+	protected int numOfPages;
+	protected boolean isTop;
+	protected String lastPostInfo;
+	
+	//add isLocked field..
+	//parser not finished for that one
+	
+	//isFavorite also..
+	
+}
